74de3e6512da765f033c70a4079549d8a2f2e888
Samirul Alam <samirulalam372@gmail.com>
2021-02-09 17:12:30 +0600

    long way to go

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let num=1;
 let index=0;
+let answer=true;
+let score=0;
+let selected=true;
 let questions=[
     {
         question:"What is your name?",
@@ -15,7 +18,7 @@ let questions=[
         optionB:"2",
         optionC:"3",
         optionD:"4",
-        answer:3
+        answer:2
     },
     {
         question:"What is your gender?",
@@ -23,7 +26,7 @@ let questions=[
         optionB:"2",
         optionC:"3",
         optionD:"4",
-        answer:3
+        answer:4
     },
     {
         question:"What is your casa?",
@@ -52,4 +55,55 @@ let next=()=>{
         optionD.innerHTML=questions[index].optionD;
         index++;
     }
+}
+
+let lock=(e)=>{
+    check(e);
+    for(let i=1;i<5;i++){
+        document.getElementById(i).classList.remove('btn-secondary');
+    }
+    document.getElementById(e).classList.add('btn-secondary');
+}
+
+let check=(e)=>{
+    if(num==1 && e==3){
+        answer=true;
+        return e;
+    }else if(num==2 && e==questions[0].answer){
+        answer=true;
+    }else if(num==3 && e==questions[1].answer){
+        answer=true;
+    }else if(num==4 && e==questions[2].answer){
+        answer=true;
+    }else if(num==5 && e==questions[3].answer){
+        answer=true;
+    }else{
+        answer=false;
+    }
+}
+
+let showResult=()=>{
+    if(answer){
+        score++;
+    }
+    switch(num) {
+        case 1:
+            document.getElementById(3).classList.add("btn-success");
+          break;
+        case 2:
+            document.getElementById(questions[0].answer).classList.add('btn-success');
+          break;
+        case 3:
+            document.getElementById(questions[1].answer).classList.add('btn-success');
+          break;
+        case 4:
+            document.getElementById(questions[2].answer).classList.add('btn-success');
+          break;
+        case 5:
+            document.getElementById(questions[3].answer).classList.add('btn-success');
+          break;
+        default:
+          console.log("wrong")
+      }
+    
 }
