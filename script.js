0fc7ad3bb2f096ee829b62c7f5a3e300b0d91a7e
Samirul Alam <samirulalam372@gmail.com>
2021-02-09 21:55:44 +0600

    project completed

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ let num=1;
 let index=0;
 let answer=true;
 let score=0;
-let selected=true;
+let checkPressed=false;
+let lockPressed=false;
+let checked=false;
+let locked=false;
+let clicked=false;
+const winSound=new Audio("./sounds/cash.mp3");
+const lossSound=new Audio("./sounds/aww.mp3");
+const nextSound=new Audio("./sounds/Kbc.mp3");
+const lockSound=new Audio("./sounds/Kbc Option Lock Tune.mp3");
+const startSound=new Audio("./sounds/Kaun Banega Karodpati.mp3");
+
+window.onload=function(){
+    var person = prompt("Please enter your name");
+    if (person != null) {
+        startSound.play();
+    }
+}
+
+
 let questions=[
     {
         question:"What is your name?",
@@ -37,38 +55,30 @@ let questions=[
         answer:3
     }
 ]
+
+
 let questionNo=document.getElementById("questionNo");
 let question=document.getElementById("question");
 let optionA=document.getElementById("1");
 let optionB=document.getElementById("2");
 let optionC=document.getElementById("3");
 let optionD=document.getElementById("4");
-let next=()=>{
-    if(num<=4){
-        num++;
-        questionNo.innerHTML=num;
-        question.innerHTML=questions[index].question;
-        question.innerHTML=questions[index].question;
-        optionA.innerHTML=questions[index].optionA;
-        optionB.innerHTML=questions[index].optionB;
-        optionC.innerHTML=questions[index].optionC;
-        optionD.innerHTML=questions[index].optionD;
-        index++;
-    }
-}
 
-let lock=(e)=>{
-    check(e);
-    for(let i=1;i<5;i++){
-        document.getElementById(i).classList.remove('btn-secondary');
+
+let choice =(e)=>{
+    if(!locked){
+        check(e);
+        for(let i=1;i<5;i++){
+            document.getElementById(i).classList.remove('btn-secondary');
+        }
+        document.getElementById(e).classList.add('btn-secondary');
+        clicked=true;
     }
-    document.getElementById(e).classList.add('btn-secondary');
 }
 
 let check=(e)=>{
     if(num==1 && e==3){
         answer=true;
-        return e;
     }else if(num==2 && e==questions[0].answer){
         answer=true;
     }else if(num==3 && e==questions[1].answer){
@@ -83,27 +93,83 @@ let check=(e)=>{
 }
 
 let showResult=()=>{
-    if(answer){
-        score++;
+    if(locked && !checkPressed){
+        checked=true;
+        if(answer){
+            document.body.style.backgroundColor = "green";
+            score++;
+            winSound.play();
+        }else{
+            document.body.style.backgroundColor = "red";
+            lossSound.play();
+        }
+
+        for(let i=1;i<5;i++){
+            document.getElementById(i).classList.add('btn-danger');
+        }
+
+        switch(num) {
+            case 1:
+                document.getElementById(3).classList.remove('btn-danger');
+                document.getElementById(3).classList.add("btn-success");
+            break;
+            case 2:
+                document.getElementById(questions[0].answer).classList.remove('btn-danger');
+                document.getElementById(questions[0].answer).classList.add('btn-success');
+            break;
+            case 3:
+                document.getElementById(questions[1].answer).classList.remove('btn-danger');
+                document.getElementById(questions[1].answer).classList.add('btn-success');
+            break;
+            case 4:
+                document.getElementById(questions[2].answer).classList.remove('btn-danger');
+                document.getElementById(questions[2].answer).classList.add('btn-success');
+            break;
+            case 5:
+                document.getElementById(questions[3].answer).classList.remove('btn-danger');
+                document.getElementById(questions[3].answer).classList.add('btn-success');
+            break;
+            default:
+                console.log("wrong")
+        }
+        checkPressed=true;
     }
-    switch(num) {
-        case 1:
-            document.getElementById(3).classList.add("btn-success");
-          break;
-        case 2:
-            document.getElementById(questions[0].answer).classList.add('btn-success');
-          break;
-        case 3:
-            document.getElementById(questions[1].answer).classList.add('btn-success');
-          break;
-        case 4:
-            document.getElementById(questions[2].answer).classList.add('btn-success');
-          break;
-        case 5:
-            document.getElementById(questions[3].answer).classList.add('btn-success');
-          break;
-        default:
-          console.log("wrong")
-      }
     
+}
+
+let lock=()=>{
+    if(clicked && !lockPressed){
+        locked=true;
+        lockSound.play();
+        lockPressed=true;
+    }
+
+}
+
+let next=()=>{
+    document.body.style.backgroundColor = "white";
+    for(let i=1;i<5;i++){
+        document.getElementById(i).classList.remove('btn-danger');
+        document.getElementById(i).classList.remove('btn-success');
+        document.getElementById(i).classList.remove('btn-secondary');
+    }
+    if(checked && num<=4){
+        num++;
+        questionNo.innerHTML=num;
+        question.innerHTML=questions[index].question;
+        question.innerHTML=questions[index].question;
+        optionA.innerHTML=questions[index].optionA;
+        optionB.innerHTML=questions[index].optionB;
+        optionC.innerHTML=questions[index].optionC;
+        optionD.innerHTML=questions[index].optionD;
+        index++;
+        nextSound.play();
+    }else if(num==5){
+        alert(score);
+    }
+    checked=false;
+    clicked=false;
+    locked=false;
+    checkPressed=false;
+    lockPressed=false;
 }
